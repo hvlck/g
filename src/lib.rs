bb0eb1c0b82e753c085aeda2ad89e6fb2b24aa7d
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-01 19:15:55 -0500

    chore: pest parsing init

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+#[macro_use]
+// std
+
+// external
+use pest::Parser;
+use pest_derive::*;
+
+// local
+
+#[derive(Parser)]
+#[grammar = "lib.pest"]
+pub struct GParser;
+
 #[cfg(test)]
 mod tests {
     #[test]
