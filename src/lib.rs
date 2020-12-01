12f01aa6c60feec36798b98b482e0794dae0459a
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-30 19:30:45 -0500

    feat: add basic let declarations and valid variable naming

--- src/lib.rs
+++ src/lib.rs
@@ -24,11 +24,26 @@ mod tests {
     fn test_number() {
         let num = GParser::parse(Rule::number, "1");
         println!("Number: {:?}", num);
+
+        let larger_num = GParser::parse(Rule::number, "9999999999");
+        println!("Larger number: {:?}", larger_num);
     }
 
     #[test]
     fn test_expression() {
         let exp = GParser::parse(Rule::expression, "1+1");
         println!("Expression: {:?}", exp);
+    }
+
+    #[test]
+    fn test_valid_variable_names() {
+        let test = GParser::parse(Rule::valid_variable_name, "generic_name");
+        println!("Valid variable name: {:?}", test);
+    }
+
+    #[test]
+    fn test_let_variable() {
+        let variable = GParser::parse(Rule::let_variable, "let generic_name = 22");
+        println!("let: {:?}", variable);
     }
 }
