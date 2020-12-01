081fbff1b825ba28eb44fd9c0f07c2f22c2c1102
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-30 20:50:08 -0500

    feat(compiler/parser): add doc comment, improve comment parsing

--- src/lib.rs
+++ src/lib.rs
@@ -17,16 +17,13 @@ mod tests {
     #[test]
     fn test_operation() {
         let op = GParser::parse(Rule::operation, "+");
-        println!("Operation: {:?}", op);
     }
 
     #[test]
     fn test_number() {
         let num = GParser::parse(Rule::number, "1");
-        println!("Number: {:?}", num);
 
         let larger_num = GParser::parse(Rule::number, "9999999999");
-        println!("Larger number: {:?}", larger_num);
 
         assert!(num.is_ok());
         assert!(larger_num.is_ok());
@@ -35,49 +32,42 @@ mod tests {
     #[test]
     fn test_expression() {
         let exp = GParser::parse(Rule::expression, "1+1");
-        println!("Expression: {:?}", exp);
         assert!(exp.is_ok());
     }
 
     #[test]
     fn test_string() {
         let string = GParser::parse(Rule::string, "\"This is an example string.\"");
-        println!("String: {:?}", string);
         assert!(string.is_ok());
     }
 
     #[test]
     fn test_valid_variable_names() {
         let test = GParser::parse(Rule::valid_variable_name, "generic_age");
-        println!("Valid variable name: {:?}", test);
         assert!(test.is_ok());
     }
 
     #[test]
     fn test_let_variable() {
         let variable = GParser::parse(Rule::let_variable, "let generic_age = 22");
-        println!("let: {:?}", variable);
         assert!(variable.is_ok());
     }
 
     #[test]
     fn test_man_variable() {
         let variable = GParser::parse(Rule::man_variable, "man \"Decay Rate\" as decay_rate = 22");
-        println!("man: {:?}", variable);
         assert!(variable.is_ok());
     }
 
     #[test]
     fn test_const_variable() {
         let variable = GParser::parse(Rule::const_variable, "const generic_age = 22");
-        println!("const: {:?}", variable);
         assert!(variable.is_ok());
     }
 
     #[test]
     fn test_use_statement() {
         let use_with_std = GParser::parse(Rule::import_pkg, "use \"math\"");
-        println!("Use: {:?}", use_with_std);
         assert!(use_with_std.is_ok());
     }
 
@@ -91,6 +81,12 @@ mod tests {
     #[test]
     fn test_process_comment() {
         let valid_comment = GParser::parse(Rule::process_comment, "/// Add 2 to 2 ///");
+        assert!(valid_comment.is_ok());
+    }
+
+    #[test]
+    fn test_doc_comment() {
+        let valid_comment = GParser::parse(Rule::doc_comment, "//! Here is some documentation. //");
         assert!(valid_comment.is_ok());
     }
 }
