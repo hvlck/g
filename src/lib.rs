6fc285cdd9009034ebc421d16717b64c3a5e6851
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-30 19:35:26 -0500

    feat(compiler): add string type to grammar

--- src/lib.rs
+++ src/lib.rs
@@ -27,23 +27,36 @@ mod tests {
 
         let larger_num = GParser::parse(Rule::number, "9999999999");
         println!("Larger number: {:?}", larger_num);
+
+        assert!(num.is_ok());
+        assert!(larger_num.is_ok());
     }
 
     #[test]
     fn test_expression() {
         let exp = GParser::parse(Rule::expression, "1+1");
         println!("Expression: {:?}", exp);
+        assert!(exp.is_ok());
+    }
+
+    #[test]
+    fn test_string() {
+        let string = GParser::parse(Rule::string, "\"This is an example string.\"");
+        println!("String: {:?}", string);
+        assert!(string.is_ok());
     }
 
     #[test]
     fn test_valid_variable_names() {
         let test = GParser::parse(Rule::valid_variable_name, "generic_name");
         println!("Valid variable name: {:?}", test);
+        assert!(test.is_ok());
     }
 
     #[test]
     fn test_let_variable() {
         let variable = GParser::parse(Rule::let_variable, "let generic_name = 22");
         println!("let: {:?}", variable);
+        assert!(variable.is_ok());
     }
 }
