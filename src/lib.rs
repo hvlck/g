a4a2f6dc24efb166d33f1048b15229b3265267dc
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-30 19:13:27 -0500

    feat(compiler): add basic math operators and number type

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,22 @@ pub struct GParser;
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_operation() {
+        let op = GParser::parse(Rule::operation, "+");
+        println!("Operation: {:?}", op);
+    }
+
+    #[test]
+    fn test_number() {
+        let num = GParser::parse(Rule::number, "1");
+        println!("Number: {:?}", num);
+    }
+
+    #[test]
+    fn test_expression() {
+        let exp = GParser::parse(Rule::expression, "1+1");
+        println!("Expression: {:?}", exp);
     }
 }
